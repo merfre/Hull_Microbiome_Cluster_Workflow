77cbff3e6156d20ee4e2cb743999b002c7420e1c
merfre <m.n.freiheit-2020@hull.ac.uk>
2023-12-05 15:48:17 +0000

    removed downstream as an option

--- workflow/rules/scripts/taxonomy_heatmaps_kraken.R
+++ workflow/rules/scripts/taxonomy_heatmaps_kraken.R
@@ -4,9 +4,178 @@ library(ggplot2)
 library("RColorBrewer")
 library("gplots")
 
-## Load files
+## Load and transform data
 
-sppno = snakemake@config[["prevalence"]]
+kraken2_results <- read.table(file = "/Users/merid/Downloads/kraken2_unassem_allsamples.tsv", sep = '\t', header = TRUE, row.names = 1)
+
+tax_level = "genus_species"
+
+sppno = "25"
+
+kraken2_tax_level <- select(kraken2_results, ends_with("kraken_report"), all_of(tax_level))
+# Separate samples and genus and species levels
+
+tax_level_pos <- ncol(kraken2_tax_level)
+# Determine the column number of the taxonomy level
+
+kraken2_tax_level <- kraken2_tax_level[!(is.na(kraken2_tax_level[tax_level_pos]) | kraken2_tax_level[tax_level_pos]==" "),]
+
+row.names(kraken2_tax_level) <- make.names(kraken2_tax_level[,tax_level], unique=TRUE)
+
+columns_remove <- c(which(colnames(kraken2_tax_level)==tax_level))
+
+kraken2_tax_level <- kraken2_tax_level[-columns_remove]
+
+if(ncol(kraken2_tax_level) > 1)
+{
+  kraken2_tax_level <- kraken2_tax_level[,order(colnames(kraken2_tax_level))]
+  # order the columns by sample names for colors on plot
+} else
+{
+  kraken2_tax_level <- kraken2_tax_level
+}
+
+
+
+
+## Determine most abundant species
+
+sortspecies <- sort(data.frame(t(kraken2_tax_level)), decreasing=TRUE)
+
+sort(kraken2_tax_level, decreasing=TRUE)
+
+top_species <- data.frame(sortspecies[1:sppno])
+
+top_names <- colnames(top_species)
+
+species_names <- colnames(sortspecies)
+
+## Heatmap
+
+top_abun <- data.frame(t(results))
+
+top_abun <- as.matrix(top_abun[colnames(top_abun) %in% top_names])
+
+lwid = c(1,3)
+lhei = c(1,3,1)
+lmat = rbind(c(0,3),c(2,1),c(0,4))
+# Moves the key to the bottom of the figure and ensures the whole title and heatmap are included
+
+pdf("/Users/merid/Downloads/test_heatmap.pdf", width=25,height=20)
+
+heatmap.2(t(data.matrix(top_abun)), col = brewer.pal(n = 9, name = "Blues"),
+          main = paste("Top", sppno, "species identified by", assigner, sep=" "),
+          margins = c(10,13),lmat = lmat, lwid = lwid, lhei = lhei, labCol = FALSE,
+          key=T, key.title = "",key.ylab = "", key.xlab = "Read counts", xlab=rownames(top_abun),
+          trace="none", Rowv=FALSE, Colv=FALSE,
+          cexCol = 1, cexRow = 1, srtCol = 45)
+
+dev.off()
+
+
+
+abun <- kraken2_tax_level
+
+colnames(abun) <- ""
+
+lwid = c(1,3)
+lhei = c(1,3,1)
+lmat = rbind(c(0,3),c(2,1),c(0,4))
+# Moves the key to the bottom of the figure and ensures the whole title and heatmap are included
+
+pdf("/Users/merid/Downloads/test_heatmap.pdf", width=25,height=20)
+
+heatmap.2(t(data.matrix(cbind(abun[1], abun[1]))), col = brewer.pal(n = 9, name = "Blues"),
+          main = paste("Species heatmap from Kraken2 results"),
+          margins = c(10,13),lmat = lmat, lwid = lwid, lhei = lhei, labRow = FALSE,
+          key=T, key.title = "",key.ylab = "", key.xlab = "Read abundance", ylab=colnames(kraken2_tax_level),
+          trace="none", Rowv=FALSE, Colv=FALSE,
+          cexCol = 1, cexRow = 1, srtCol = 45)
+
+dev.off()
+
+
+lwid = c(1,3)
+lhei = c(1,3,1)
+lmat = rbind(c(0,3),c(2,1),c(0,4))
+# Moves the key to the bottom of the figure and ensures the whole title and heatmap are included
+
+heatmap.2(t(data.matrix(kraken2_tax_level)), col = brewer.pal(n = 9, name = "Blues"),
+          main = paste("Species heatmap from Kraken2 results"),
+          margins = c(10,13),lmat = lmat, lwid = lwid, lhei = lhei, labCol = FALSE,
+          key=T, key.title = "",key.ylab = "", key.xlab = "Read counts", xlab=rownames(kraken2_tax_level),
+          trace="none", Rowv=FALSE, Colv=FALSE,
+          cexCol = 1, cexRow = 1, srtCol = 45)
+
+
+
+
+
+
+if (nrow(results) >= sppno)
+{
+  ## Determine most abundant species
+  
+  sortspecies <- sort(data.frame(t(results)), decreasing=TRUE)
+  
+  top_species <- data.frame(sortspecies[1:sppno])
+  
+  top_names <- colnames(top_species)
+  
+  species_names <- colnames(sortspecies)
+  
+  ## Heatmap
+  
+  top_abun <- data.frame(t(results))
+  
+  top_abun <- as.matrix(top_abun[colnames(top_abun) %in% top_names])
+  
+  lwid = c(1,3)
+  lhei = c(1,3,1)
+  lmat = rbind(c(0,3),c(2,1),c(0,4))
+  # Moves the key to the bottom of the figure and ensures the whole title and heatmap are included
+  
+  pdf(out_path, width=25,height=20)
+  
+  heatmap.2(t(data.matrix(top_abun)), col = brewer.pal(n = 9, name = "Blues"),
+            main = paste("Top", sppno, "species identified by", assigner, sep=" "),
+            margins = c(10,13),lmat = lmat, lwid = lwid, lhei = lhei, labCol = FALSE,
+            key=T, key.title = "",key.ylab = "", key.xlab = "Read counts", xlab=rownames(top_abun),
+            trace="none", Rowv=FALSE, Colv=FALSE,
+            cexCol = 1, cexRow = 1, srtCol = 45)
+  
+  dev.off()
+} else (nrow(results) < sppno)
+{
+  ## Heatmap
+  
+  abun <- results
+  
+  colnames(abun) <- ""
+  
+  lwid = c(1,3)
+  lhei = c(1,3,1)
+  lmat = rbind(c(0,3),c(2,1),c(0,4))
+  # Moves the key to the bottom of the figure and ensures the whole title and heatmap are included
+  
+  pdf(out_path, width=25,height=20)
+  
+  heatmap.2(t(data.matrix(cbind(abun[1], abun[1]))), col = brewer.pal(n = 9, name = "Blues"),
+            main = paste("Species identified by", assigner, sep=" "),
+            margins = c(10,13),lmat = lmat, lwid = lwid, lhei = lhei, labRow = FALSE,
+            key=T, key.title = "",key.ylab = "", key.xlab = "Read counts", ylab=colnames(results),
+            trace="none", Rowv=FALSE, Colv=FALSE,
+            cexCol = 1, cexRow = 1, srtCol = 45)
+  
+  dev.off()
+}
+
+
+### Plot creation
+
+taxonomy_heatmap(snakemake@input[[1]], "Kraken2", '_kraken', snakemake@output[[1]])
+
+
 
 ### Defining function
 
